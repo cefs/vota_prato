118cdc48f6f5e3a5ddacb34c416bb4c11c7d8f8a
CARLOS SILVA <cefs1984@gmail.com>
2013-01-25 01:26:02 -0200

    chapter 7.18

---
+++ app/models/cliente.rb
@@ -0,0 +1,13 @@
+#encoding: utf-8
+class Cliente < ActiveRecord::Base
+	attr_accessible :nome, :idade
+
+	validates_presence_of :nome,	message: "- deve ser preenchido"
+	validates_presence_of :idade,	message: "- deve ser preenchido"
+
+	validates_numericality_of	:idade, 
+								greater_than: 0,
+								less_than: 100,
+								message: "- deve ser um número entre 0 e 100"
+
+end
