45e2dca9bab385fa2f6d0aa7dd7ff39a4d4ed135
CARLOS SILVA <cefs1984@gmail.com>
2013-01-29 13:25:02 -0200

    chapter 13.3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
+	def comentarios(comentavel)
+		comentarios = "<div id='comentarios'>"
+			comentarios << "<h3>Comentarios</h3>"
+			comentarios << render(comentavel.comentarios) unless comentavel.comentarios.empty?
+		comentarios << "</div>"
+		raw comentarios
+	end
+
 	def valor_formatado(number)
 		number_to_currency(number, :unit => "R$ ",:separator =>",", :delimiter => ".")
 	end
 
 	def menu_principal
-		menu = %w(cliente qualificacao restaurante)
+		menu = %w(cliente qualificacao restaurante comentario)
 		menu_principal = "<ul>"
 			menu.each do |item|
 				menu_principal << "<li>"
